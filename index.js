92fb2a209c3c4bd30e132cd93fe23fefc0ce4591
trt44722 <thomas.trottmann@yahoo.de>
2017-12-28 21:20:48 +0100

    delete server.bind and readme

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const Hapi = require('hapi');
 
 var server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 4000 });
-//server.bind({
- // currentUser: {},
-  //users: {},
-  //donations: [],
-//});
+
 require('./app/models/db');
 
 server.register([require('inert'), require('vision'), require('hapi-auth-cookie')], err => {
